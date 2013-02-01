b47673e5d0ba1fc720e10efa2f67e437fa673412
Tom Conroy <tom@conroy.com.au>
2013-02-01 14:51:14 +1000

    Diagram labels

--- app/pdfs/diagram.rb
+++ app/pdfs/diagram.rb
@@ -26,8 +26,8 @@ class Diagram
     
     data = [
       {
-        :text => "Solar Modules\n",
-        :styles => [:bold]
+        :text => "Solar Modules -\n",
+        :styles => [:regular]
       },
       {
         :text => "#{@manual.panels_number} x #{@manual.panels_brand} 245W",
@@ -77,7 +77,7 @@ class Diagram
       @doc.line_to [left - 20, @top - 20]
     end
     
-
+    
     # lines into corner
     left = left - 30
     top_left = [left - 100 - gap, @top]
@@ -85,36 +85,39 @@ class Diagram
       # outer line
       @doc.move_to [left, @top]
       @doc.line_to top_left
-      @doc.line_to [left - 100 - gap, @top - gap - 60]
+      @doc.line_to [left - 100 - gap, @top - gap - 70]
       
       # inner line
       @doc.move_to [left, @top - gap]
       @doc.line_to [left - 100, @top - gap]
-      @top = @top - gap - 60
+      @top = @top - gap - 70
       @doc.line_to [left - 100, @top]
     end
+    
+    @doc.text_box "2 x 1000V DC Isolators", :at => [left - 70, @top + 20]
     
     # second switch
     left = left - 100
     @doc.fill_ellipse [left, @top], 4
     @doc.fill_ellipse [left - gap, @top], 4
     @doc.stroke do
-      # inner line
+      # inner line diagonal
       @doc.move_to [left, @top]
       @doc.line_to [left - 20, @top - 20]
-      # outer line
+      # outer line diagonal
       @doc.move_to [left - gap, @top]
       @doc.line_to [left - gap - 20, @top - 20]
       
-      # inner
-      @top = @top - 30
+      # inner lines to inverter
+      @top = @top - 20
       @doc.move_to [left, @top]
-      @doc.line_to [left, @top - 50]
+      @doc.line_to [left, @top - 30]
       
       @doc.move_to [left - gap, @top]
-      @doc.line_to [left - gap, @top - 50]
-      @top = @top - 50
+      @doc.line_to [left - gap, @top - 30]
+      @top = @top - 30
     end
+    
   end
   
   def inverter
@@ -130,6 +133,28 @@ class Diagram
       @doc.move_to [@left + 50, @top - 20]
       @doc.line_to [@left + 115, @top - 20]
     end
+    
+    
+    text = [
+      {
+        :text => "Inverter -\n",
+        :styles => [:regular]
+      },
+      {
+        :text => "#{@manual.inverter_brand} #{@manual.inverter_model}\n",
+        :styles => [:bold]
+      },
+      {
+        :text => "Output: ",
+        :styles => [:regular]
+      },
+      {
+        :text => "#{@manual.inverter_output}kW",
+        :styles => [:bold]
+      }
+    ]
+    
+    @doc.formatted_text_box text, :at => [@left + width + 50, @top - 10]
     
     # DC dash
     @doc.dash 5
@@ -149,7 +174,8 @@ class Diagram
       @doc.curve_to [curve_left + 60, @top - 60], :bounds => [[curve_left + 50, @top - 80], [curve_left + 50, @top - 60]]
     end
     
-    @top = @top - height
+    @top = @top - height    
+    
   end
   
   def switch_board
@@ -157,14 +183,33 @@ class Diagram
     # single line
     @doc.stroke do
       @doc.move_to [left, @top]
-      @doc.line_to [left, @top - 50]
-      @doc.rectangle [left - 10, @top - 50], 20, 50
-      @doc.move_to [left, @top - 100]
-      @doc.line_to [left, @top - 150]
-      @doc.rectangle [@left + 30, @top - 150], 160, 100
+      @doc.line_to [left, @top - 80]
+      @doc.rectangle [left - 10, @top - 80], 20, 50
+      @doc.move_to [left, @top - 130]
+      @doc.line_to [left, @top - 210]
+      @doc.rectangle [@left + 30, @top - 210], 160, 100
     end
     
-    @doc.text_box "Switch Board", :style => :bold, :at => [@left + 40, @top - 195], :width => 140, :align => :center
+    @doc.text_box "Switch Board", :style => :bold, :at => [@left + 40, @top - 255], :width => 140, :align => :center
+    
+    label1 = "WARNING DC VOLTAGE\nOPEN CIRCUIT VOLTAGE\nSHORT CIRCUIT CURRENT"
+    @doc.text_box label1, :at => [@left + 300, @top - 140], :style => :bold, :align => :center, :width => 200
+    @doc.stroke do
+      @doc.rectangle [@left + 300, @top - 125], 200, 75
+    end
+    
+    label2 = "WARNING DUAL SUPPLY\nISOLATE BOTH SOLAR AND NORMAL SUPPLY BEFORE WORKING ON SWITCHBOARD"
+    @doc.text_box label2, :at => [@left + 300, @top - 230], :width => 200, :style => :bold, :align => :center
+    @doc.stroke do
+      @doc.rectangle [@left + 300, @top - 215], 200, 95
+    end
+    
+    label3 = "SOLAR\nSUPPLY\nMAIN\nSWITCH"
+    @doc.text_box label3, :at => [left + 40, @top - 70], :align => :center, :width => 80, :style => :bold
+    @doc.stroke do
+      @doc.rectangle [left + 40, @top - 55], 80, 95
+    end
+    
   end
   
 end
