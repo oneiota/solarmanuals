c347c5e01e03beda87ebff688ad2c609cf856018
Tom Conroy <tom@conroy.com.au>
2013-02-06 14:38:17 +1000

    Logger set to STDOUT

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,8 @@ Solar::Application.configure do
 
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = Logger.new(STDOUT) 
+  config.log_level = :info
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
