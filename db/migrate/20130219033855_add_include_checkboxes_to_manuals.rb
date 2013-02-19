03eb56d51faba24dc4ef9ce4ac2355f287108d05
Tom Conroy <tom@conroy.com.au>
2013-02-19 20:32:41 +1000

    Fix migration

--- db/migrate/20130219033855_add_include_checkboxes_to_manuals.rb
+++ db/migrate/20130219033855_add_include_checkboxes_to_manuals.rb
@@ -3,10 +3,6 @@ class AddIncludeCheckboxesToManuals < ActiveRecord::Migration
     add_column :manuals, :include_performance, :boolean, default: false
     add_column :manuals, :include_wiring, :boolean, default: false
     add_column :manuals, :include_certificate, :boolean, default: false
-    
-    Manual.all.each do |m|
-      m.update_attributes(:include_performance => true, :include_wiring => true, :include_certificate => true)
-    end
   end
   
   def down
