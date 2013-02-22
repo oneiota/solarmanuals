5677925e543aa306f947f31f7008e76c417071c2
Tom Conroy <tom@conroy.com.au>
2013-02-22 17:10:15 +1000

    Allow insiders to download

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
     can [:create, :destroy, :update, :read, :set_feature], Manual
     
     can :document, Manual do |manual|
-      manual.paid? || manual.user.subscribed?
+      manual.paid? || manual.user.subscribed? || manual.user.insider
     end
     
     can :manage, User do |other_user|
