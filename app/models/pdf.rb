6e84a953f05a78f9c19f063077ed70fe7f0ac041
Tom Conroy <tom@conroy.com.au>
2013-03-23 13:17:24 +1000

    Proper attachment errors

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -2,6 +2,9 @@ class Pdf < ActiveRecord::Base
   has_attached_file :file, PDF_OPTS
   attr_accessible :file, :user_id
   
+  validates_attachment :file,
+    :content_type => { :content_type => "application/pdf" }
+  
   belongs_to :user
   has_and_belongs_to_many :manuals
 end
