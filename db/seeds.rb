388b92f49367c51e9022df676738d0efc6ae9534
Tom Conroy <tom@conroy.com.au>
2013-03-26 16:49:05 +1000

    Change seed to not delete old

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,26 +17,28 @@ if State.count == 0
   end
 end
   
-Message.destroy_all
+if Message.count == 0
   
-messages = [
-"Make manuals faster!<br/>You can duplicate saved manuals for installs that have similar details. Simply hit 'My Installs' to view your saved manuals, then select 'Duplicate' next to the manual you wish to copy", 
-"Did you know that installers are liable to provide complete documentaion to all customers for jobs completed from October 2012 onwards"
-]
+  messages = [
+  "Make manuals faster!<br/>You can duplicate saved manuals for installs that have similar details. Simply hit 'My Installs' to view your saved manuals, then select 'Duplicate' next to the manual you wish to copy", 
+  "Did you know that installers are liable to provide complete documentaion to all customers for jobs completed from October 2012 onwards"
+  ]
 
-messages.each do |message|
-  params = { content: message }
-  Message.create! params
-  puts "created : " + message
+  messages.each do |message|
+    params = { content: message }
+    Message.create! params
+    puts "created : " + message
+  end
 end
 
-Checklist.destroy_all
+if Checklist.count == 0
 
-checklist_qs = [
-"Panel warranties","Inverter warranties","Array frame engineering certificate for wind and mechanical loading","Certificate of safety and compliance issued by system installer or designer","Commissioning sheet and installation checklist <strong class='help boldweight'><a href='http://www.solaraccreditation.com.au/installerresources/formsandchecklists.html' target='_blank' title='CEC Commissioning sheet and installation checklist'>- available for download here</a></strong>","Any additional state based requirements"
-]
+  checklist_qs = [
+  "Panel warranties","Inverter warranties","Array frame engineering certificate for wind and mechanical loading","Certificate of safety and compliance issued by system installer or designer","Commissioning sheet and installation checklist <strong class='help boldweight'><a href='http://www.solaraccreditation.com.au/installerresources/formsandchecklists.html' target='_blank' title='CEC Commissioning sheet and installation checklist'>- available for download here</a></strong>","Any additional state based requirements"
+  ]
 
-checklist_qs.each do |checklist|
-  params = { question: checklist }
-  Checklist.create! params
+  checklist_qs.each do |checklist|
+    params = { question: checklist }
+    Checklist.create! params
+  end
 end
