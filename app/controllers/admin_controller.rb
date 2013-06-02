133aaa94436fadcfcc2c80da02192dfbd9fe1b2e
Tom Conroy <tom@conroy.com.au>
2013-06-01 22:03:47 -0700

    Clean up ascii characters, add unauthorized exception

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,14 +2,14 @@
 # encoding: utf-8
 
 class AdminController < ApplicationController
+  
+  before_filter { raise CanCan::AccessDenied.new("Unauthorized") unless current_user.insider }
 
-  #before_filter { unauthorized! unless current_user.insider }
-
-    def admin_info
-      @total_users = User.count
-      @total_users_this_month = User.where('created_at >= ?', 1.month.ago)
-      @total_billed = EwayPayment.all.inject(0){|sum, payment| sum += payment.return_amount}
-      @total_billed_this_month = EwayPayment.all.inject(0){|sum, payment| sum += payment.return_amount}
-    end
-
+  def admin_info
+    @total_users = User.count
+    @total_users_this_month = User.where('created_at >= ?', 1.month.ago)
+    @total_billed = EwayPayment.all.inject(0){|sum, payment| sum += payment.return_amount}
+    @total_billed_this_month = EwayPayment.all.inject(0){|sum, payment| sum += payment.return_amount}
+  end
+  
 end
