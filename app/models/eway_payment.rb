dba7170c961d4ff5cbb6cf36d59a4073e25a4a01
Tom Conroy <conroy@typography.com>
2014-09-16 22:32:18 -0400

    Rescue eway payment errors

--- app/models/eway_payment.rb
+++ app/models/eway_payment.rb
@@ -1,93 +1,95 @@
 class EwayPayment < ActiveRecord::Base
-  
+
   attr_accessible :eway_auth_code, :eway_error, :eway_status, :return_amount, :transaction_number, :user_id
-  
+
   attr_accessor :cc_number, :cc_expiry_month, :cc_expiry_year, :cvn, :remember
-  
+
   validates_presence_of :return_amount, :transaction_number, :user_id
-  
+
   validate :transaction_approved
-  
+
   belongs_to :user
   has_many :manuals
-  
-  
+
+
   def self.process_single_payment!(user)
     @payment = EwayPayment.new
     @payment.user = user
-    
+
     # create new eway id if we don't have one yet, or they've entered new details
     if !user.eway_id || user.cc_number
       user.eway_id = user.create_eway_id
     end
-    
+
     @payment.process_payment!(CASUAL_FEE)
-    
-    if @payment.save  
+
+    if @payment.save
       # email receipt
       UserMailer.receipt(@payment).deliver
     end
-    
+
     return @payment
   end
-  
+
   def process_payment!(amount)
-    
+
     response = Eway.client.process_payment(
       user.eway_id,
       amount,
       'Solar Manuals Subscription'
     )
-    
+
     set_payment_details(response)
-    
+
   end
-  
+
   def process_subscription!
     charge_amount = user.current_charge
     return if charge_amount == 0
-    
-    self.process_payment!(charge_amount)
-    
+
+    begin
+      self.process_payment!(charge_amount)
+    rescue Error => e
+      user.flag!
+      return false
+    end
+
     self.subscription = true
-    
+
     if self.save
       puts "Created payment for #{user.full_name} at #{charge_amount}"
-      
+
       user.flagged = false
       user.last_payed_at = Time.now
       user.save
-      
+
       user.unpaid_marked_manuals.each do |manual|
         manual.eway_payment = self
         manual.save
       end
-      
+
       # email receipt
       UserMailer.receipt(self).deliver
-      
+
       return true
-      
+
     else
-      
+
       puts "Payment failed for #{user.full_name}, #{errors[:base].first}"
-      
-      # email warnings
       UserMailer.admin_payment_alert(self).deliver
       UserMailer.payment_failed_alert(self).deliver
-      
-      user.flagged = true
-      user.save
+
+      user.flag!
       return false
     end
   end
-  
+
   def process_unsubscribe_payment!
-    
+
   end
-  
+
   private
-  
+
   def set_payment_details(response)
     self.eway_error = response['ewayTrxnError']
     self.eway_status = response['ewayTrxnStatus'] == "True"
@@ -95,12 +97,12 @@ class EwayPayment < ActiveRecord::Base
     self.return_amount = response['ewayReturnAmount']
     self.eway_auth_code = response['ewayAuthCode']
   end
-  
+
   def transaction_approved
     error = self.eway_error.split(",")
     unless error.first == "00"
       errors[:base] << error.last
     end
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,143 +1,143 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-  
+
+
   attr_protected :insider, :eway_id, :subscribed
-  
+
   has_attached_file :logo, LOGO_OPTS
   validates_attachment :logo,
     :size => { :in => 0..2.megabytes }
-  
+
   attr_accessor :current_password, :cc_number, :cc_expiry_month, :cc_expiry_year, :cvn, :remember, :validate_card, :terms
-  
+
   validates :cc_number, :length => { :is => 16 }, :if => :should_validate_card?
   validates :cc_expiry_month, :length => { :is => 2 }, :if => :should_validate_card?
   validates :cc_expiry_year, :length => { :is => 2 }, :if => :should_validate_card?
   validates :cvn, :length => { :in => 3..4 }, :if => :should_validate_card?
-  
+
   def should_validate_card?
     validate_card
   end
-  
+
   validates_presence_of :first_name, :last_name
-  
+
   validate :agree_terms, :on => :create
-  
+
   def agree_terms
     unless terms == '1'
       errors[:terms] = "You must agree."
     end
   end
-  
+
   has_many :manuals
   has_many :eway_payments
-  
+
   has_many :pdfs
   accepts_nested_attributes_for :pdfs
-  
+
   has_and_belongs_to_many :messages
-  
+
   after_create :send_welcome_mail
-  
+
   def unread_message
     if messages.count > 0
       return Message.where('id NOT IN (?)', messages).first
     end
     Message.first
   end
-  
+
   def flash_message
-    if unread_message 
+    if unread_message
       return {unread_message.flashtype.to_sym => [unread_message.id, unread_message.content.html_safe]}
     end
   end
-  
+
   def active_manuals
     manuals.where(:trashed => false).order('install_date DESC').includes(:eway_payment)
   end
-  
+
   def full_name
     "#{first_name} #{last_name}"
   end
-  
+
   def full_address
     "#{company_address} #{company_suburb} #{company_postcode}"
   end
-  
+
   def subscribed?
     subscribed
   end
-  
+
   def fields_filled?
     !fields_not_filled?
   end
-  
+
   def fields_not_filled?
     [company, accreditation, abn, company_address, company_suburb, company_postcode, contact_email, company_phone].each do |field|
       return true if field.blank?
     end
     false
   end
-  
-  def update_with_password(params={}) 
-    if params[:password].blank? 
-      params.delete(:password)      
+
+  def update_with_password(params={})
+    if params[:password].blank?
+      params.delete(:password)
       params.delete(:current_password)
-      params.delete(:password_confirmation) if params[:password_confirmation].blank? 
-    end 
-    update_attributes(params) 
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+    update_attributes(params)
   end
-  
+
   def is_new?
     manuals.count == 0
   end
-  
+
   def pdfs_array=(array)
     array.each do |file|
       pdfs.build(:file => file)
     end
   end
-  
+
   def unpaid_marked_manuals
     manuals.where(:marked => true).reject(&:paid?)
   end
-  
+
   def billable_manuals(number)
     ms = number - FREE_MANUALS
     ms < 0 ? 0 : ms
   end
-  
+
   def manuals_charge(number)
     billable_manuals(number) * SUBSCRIPTION_MANUAL_FEE
   end
-  
+
   def current_charge
     charge = manuals_charge(billable_manuals(unpaid_marked_manuals.count))
     charge + SUBSCRIPTION_FEE
   end
-  
+
   # payments
-  
+
   def self.billable
     where(:subscribed => true).where('last_payed_at <= ?', SUBSCRIPTION_INTERVAL.ago).where('eway_id IS NOT NULL').where(:insider => false)
   end
-  
+
   def is_billable?
     User.billable.include?(self)
   end
-  
+
   def first_manual
     manuals.order('created_at ASC').first
   end
-  
+
   def is_first_manual?(manual)
     if manuals.count > 0
       return manual.id == first_manual.id
     end
     return true
   end
-  
+
   def create_eway_id
     self.eway_id = Eway.client.create_customer(
       'CustomerRef' => self.id,
@@ -162,20 +162,25 @@ class User < ActiveRecord::Base
       'CCExpiryMonth' => cc_expiry_month,
       'CCExpiryYear' => cc_expiry_year
     )
-    
+
     if remember == '1'
       self.save
     end
-    
+
     return self.eway_id
   end
-  
+
   def stored_cc_number
     self[:stored_cc_number] || get_cc_number
   end
-  
+
+  def flag!
+    self.flagged = true
+    self.save
+  end
+
   private
-  
+
   def get_cc_number
     if !eway_id
       return nil
@@ -185,9 +190,9 @@ class User < ActiveRecord::Base
     self.save
     return self.stored_cc_number
   end
-  
+
   def send_welcome_mail
     UserMailer.welcome(self).deliver
   end
-  
+
 end
